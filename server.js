3faf3d4aa0a3178de3a6a79a7f52251b6b3cebd5
umairaslam2 <umairaslamumer@gmail.com>
2025-12-11 18:09:35 +0500

    k8s cluster

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send(" container successfullly create hogiya ");
+  res.send(" container successfullly bangiya ");
 });
 
 app.get("/healthz", (req, res) => {
